177b2de9bce2d7c53b4054bf5ac17990f8b00df2
AlexanderQA <ip.boroday@gmail.com>
2025-02-27 23:14:19 +0300

    Tuchs sucks

--- src/main/java/SearchTests.java
+++ src/main/java/SearchTests.java
@@ -1,11 +1,14 @@
 import org.junit.jupiter.api.Test;
 
-import static  com.codeborne.selenide.Condition.text;
-import static  com.codeborne.selenide.Condition.$;
-import static  com.codeborne.selenide.Condition.open;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
-
-public class SearchTests {
+class SearchTests {
     @Test
-    void
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("body > div:nth-child(1) > div").shouldHave(text("URL: https://www.google.com/search?"));
+    }
 }
