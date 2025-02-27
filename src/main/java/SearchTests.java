821db773514ebeea069b5ea84eceac3d3360b65d
AlexanderQA <ip.boroday@gmail.com>
2025-02-27 22:58:25 +0300

    Tuchs sucks

---
+++ src/main/java/SearchTests.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static  com.codeborne.selenide.Condition.text;
+import static  com.codeborne.selenide.Condition.$;
+import static  com.codeborne.selenide.Condition.open;
+
+
+public class SearchTests {
+    @Test
+    void
+}
